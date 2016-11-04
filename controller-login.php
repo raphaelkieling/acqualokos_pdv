617551c4a98ffe241363b81eedf98fe282ed8bc5
Raphael Kieling <rapha_kieling@hotmail.com>
2016-11-04 13:55:16 -0200

    md5

---
+++ controller-login.php
@@ -0,0 +1,31 @@
+<?php
+	$conexao = mysqli_connect("localhost","root","","acqualokos_revendedor");
+	if(isset($_POST['admin_senha'])){ //3
+		$admin         = md5($_POST['admin_senha']);
+		if(logar($conexao,$admin,3)){
+			header("location:admin_login_acess.php");
+		}
+	}
+	
+	if(isset($_POST['revendedor_senha'])){//1
+		$revendedor = md5($_POST['revendedor_senha']);
+		if(logar($conexao,$revendedor,1)){
+			header("location:revendedor_login_acess.php");
+		}
+		
+	}
+
+	if(isset($_POST['acqua_senha'])){//2
+		$acqualokos =md5( $_POST['acqua_senha']);
+		if(logar($conexao,$acqualokos,2)){
+			header("location:acqualokos_login_acess.php");
+		}
+	}
+
+	function logar($conexao,$senha,$tipo){
+		$select         = mysqli_query($conexao,"select * from banco_senhas where senha='$senha' and tipo_entrada=$tipo"); 
+		if(mysqli_fetch_row($select)>0){
+			return true;
+		}
+	}
+?>
---
+++ revendedor_login_acess.php
@@ -0,0 +1,11 @@
+	<?php include('views/head.php') ?>
+<body>
+	<!-- Header -->
+	<?php include('views/header.php') ?>
+	<div class="container main">
+		<br><br>
+		<form action="confirmação.php" method="post">
+		<h1>Revisão de Pontos de Venda para Confirmação</h1>
+	</div>
+</body>
+</html>
