a371f670f8f33d0841d6f4d88d723558f0de4fc3
Raphael Kieling <rapha_kieling@hotmail.com>
2017-01-02 15:27:35 -0200

    CSS modificado para ficar responsivo os logins, tela index modificada para a nova função

--- index.php
+++ index.php
@@ -14,14 +14,36 @@ include('views/head.php')
 	<div class="container main">
 		<?php include("views/erro.php");?>
 		<h1><center>Trabalha conosco?</center></h1>
-		<div class="login-acesso">
-			<img src="img/logo-acqua-lokos.png" alt="">
-			<form action="controller-login.php" method="post">
-				<center>Adquira seu url e senha com o acqua lokos</center>
+		<div class="row">
+			<div class="login-acesso col-md-5">
+				<img src="img/logo-acqua-lokos.png" alt="">
+				<h4><center>Adquira seu url e senha com o acqua lokos</center></h4>
 				<!-- <input type="password" name="senha_total" class="form-controlado" placeholder="Senha" readonly> -->
 				<!-- <button class="btn btn-sucesso form-controlado">Acessar</button> -->
-			</form>
+
+				<br><br>
+				<center>Quer vir aproveitar? Verifique se você já foi aceito!</center>
+				<div class="alert alert-warning">
+					<p>
+						Coloque CPF - RG. Caso não tenha cadastrado um desses documentos não é possível fazer a verificação, pois apenas estes são números únicos. 
+					</p>
+					<p>
+						NOMES NÃO são verificados, pois podem ocorrer falhas ortográficas por parte do cadastrante.
+					</p>
+				</div>
+				<input type="text" class="form-control" placeholder="CPF - RG">
+				<button class="btn btn-warning form-control"><span class="glyphicon glyphicon-search"></span>Verificar Disponibilidade</button>
+				<br> <br>
+				<div class="alert alert-success">
+					Foi aceito! Vem aproveitar!
+				</div>
+				<div class="alert alert-danger">
+					<p>Desculpa, mas teu CPF ou RG não consta no sistema. :(</p>
+					<p>Aguarde a confirmação do Revendedor!</p>
+				</div>
+			</div>
 		</div>
 		<center style="margin-top:40px;"><small>Está com problemas? Ligue para (51) 9412-1300</small></center>
+		<br>
 	</div>
 </body>
