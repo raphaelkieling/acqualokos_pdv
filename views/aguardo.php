d4e90cfb711ee7ded4b58399a9e3b76f70191a5f
Raphael Kieling <rapha_kieling@hotmail.com>
2017-01-04 17:19:39 -0200

    Botão deleta em espera tambem

--- views/aguardo.php
+++ views/aguardo.php
@@ -1,7 +1,8 @@
 				<tr>
 				<?php 
 					if($listas['status']==0){						
-						echo "<td colspan='9' class='aguardo'><center><div class='label label-warning '>Aguardando Aprovação do Acqua Lokos</div></center></td>";
+						echo "<td colspan='9' class='aguardo'><center><div class='label label-warning '>Aguardando Aprovação do Acqua Lokos</div></center><div onclick='btnCancela(".$listas['id'].");' class='btn btn-danger form-control'>Cancelar</button></div></td>";
+												
 					}else if($listas['status']==1){
 						echo "<td colspan='9'><center><div class='label label-success'>Lista Confirmada!</div></center></td>";
 					}else{
