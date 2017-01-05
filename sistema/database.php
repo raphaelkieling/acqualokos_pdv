ced343b0b112ec39fb828f3388de8918d4021b5a
Raphael Kieling <rapha_kieling@hotmail.com>
2017-01-05 14:51:53 -0200

    database add

---
+++ sistema/database.php
@@ -0,0 +1,22 @@
+<?php 
+	#Config banco de dados	
+		//---HOSTINGER----
+		// nome Banco:u172364612_ pdv
+		// nome: u172364612_ acqua
+		// senha : admacqua
+	//$db = array(
+	//	"local" => "localhost",
+	//	"usuario" => "root",
+	//	"senha"=> "",
+	//	"banco"=>"acqualokos_revendedor"
+	//);
+
+	$db = array(
+		"local" => "localhost",
+		"usuario" => "u172364612_acqua",
+		"senha"=> "admacqua",
+		"banco"=>"u172364612_pdv"
+	);
+	
+	$conexao = mysqli_connect($db['local'],$db['usuario'],$db['senha'],$db['banco']);
+?>
