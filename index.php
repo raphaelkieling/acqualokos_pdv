c4bf0fd6703faafc7076ad4d6d174d6d2f72b741
Raphael Kieling <rapha_kieling@hotmail.com>
2017-01-30 15:57:23 -0200

    Chat no index.php

--- index.php
+++ index.php
@@ -76,5 +76,9 @@ include('views/head.php')
 		<center style="margin-top:40px;"><small>Está com problemas? Ligue para (51) 9412-1300</small></center>
 		<br>
 	</div>
+	<script type='text/javascript'>
+		(function(){ var widget_id = 'Al8MOHZTRG';var d=document;var w=window;function l(){
+		var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+	</script>
 	<?php include("views/footer.php"); ?>
 </body>
